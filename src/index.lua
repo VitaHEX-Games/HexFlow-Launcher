b06ba24c48549f2bf69088f5dddc631e246591f4
jimbob4000 <71288909+jimbob4000@users.noreply.github.com>
2022-09-18 22:51:12 +0100

    Update index.lua

--- src/index.lua
+++ src/index.lua
@@ -6,7 +6,7 @@
 
 dofile("app0:addons/threads.lua")
 local working_dir = "ux0:/app"
-local appversion = "4.1"
+local appversion = "4.0.1"
 function System.currentDirectory(dir)
     if dir == nil then
         return working_dir
